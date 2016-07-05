10467b273c9e51c23babf49e84b6f20f3c63a7c8
Wilco Wolters <wilcowolters@gmail.com>
2016-07-05 20:25:18 +0200

    Add PUT and DELETE methods to BaseController

--- src/main/java/com/move4mobile/bite/controller/BaseController.java
+++ src/main/java/com/move4mobile/bite/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.move4mobile.bite.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.move4mobile.bite.exception.BadRequestException;
 import com.move4mobile.bite.model.BaseEntity;
 import com.move4mobile.bite.repository.BaseRepository;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 import static com.move4mobile.bite.support.Validate.notNull;
 
@@ -36,6 +38,20 @@ public abstract class BaseController<T extends BaseEntity> {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public T get(@PathVariable("id") T t) {
         return notNull(t, "value");
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public T change(@PathVariable("id") T idProvider, @Valid @RequestBody T t) {
+        if (!Objects.equals(notNull(idProvider, "id").getId(), t.getId())) {
+            throw new BadRequestException("id in path and in object should be equal");
+        }
+        return repository.saveAndFlush(t);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public T delete(@PathVariable("id") T t) {
+        repository.delete(notNull(t, "id"));
+        return t;
     }
 
 }
---
+++ src/main/java/com/move4mobile/bite/exception/BadRequestException.java
@@ -0,0 +1,16 @@
+package com.move4mobile.bite.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by Wilco Wolters on 05/07/2016.
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
