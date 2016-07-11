5e2450ae1a86989fae9b11f16933c4aef01e22f8
Wilco Wolters <wilcowolters@gmail.com>
2016-07-11 20:15:45 +0200

    Add baseProductType to store

--- src/main/java/com/move4mobile/bite/model/Store.java
+++ src/main/java/com/move4mobile/bite/model/Store.java
@@ -1,6 +1,7 @@
 package com.move4mobile.bite.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.move4mobile.bite.resolver.EntityIdResolver;
@@ -10,6 +11,7 @@ import lombok.Setter;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -35,6 +37,13 @@ public class Store extends BaseEntity {
 
     @OneToMany(mappedBy = "store", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<StoreProduct> products = new ArrayList<>();
+
+    @Getter
+    @Setter
+    @NotNull
+    @ManyToOne
+    @JsonIdentityReference(alwaysAsId = true)
+    private ProductType baseProductType;
 
     public List<StoreProduct> getProducts() {
         return new ArrayList<>(products);
