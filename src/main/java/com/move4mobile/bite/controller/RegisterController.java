06a85f1fd72eb20a5ef1289282ed372dbd768121
Wilco Wolters <wilcowolters@gmail.com>
2016-07-25 21:18:42 +0200

    Fix response of register endpoint

--- src/main/java/com/move4mobile/bite/controller/RegisterController.java
+++ src/main/java/com/move4mobile/bite/controller/RegisterController.java
@@ -1,12 +1,11 @@
 package com.move4mobile.bite.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.move4mobile.bite.model.BaseEntity;
 import com.move4mobile.bite.model.User;
 import com.move4mobile.bite.service.BaseService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -22,6 +21,8 @@ public class RegisterController {
     private BaseService<User> userService;
 
     @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @JsonView(BaseEntity.DefaultView.class)
     public User register(@Valid @JsonView(User.RegisterView.class) @RequestBody User user) {
         return userService.store(user);
     }
